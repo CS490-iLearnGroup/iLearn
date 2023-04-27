ff83c061ed39a17d19128b0a108b61984b1bbace
smp0714 <smp89@njit.edu>
2023-04-26 22:46:33 -0400

    General Update

---
+++ moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'pNn0RYVWEZ2CELtSsPr1uFUQzJ7vNh1Vlocalhost';
+$CFG->bootstraphash = 'fc92a21b6c819e4e9d8f92489e03bd70';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
